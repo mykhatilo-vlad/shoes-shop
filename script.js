7642d9fdc7dce0ed786d80f2cb31d503f9e215e2
Vlad Mykhatilo <mykhatilo.vlad@gmail.com>
2024-08-19 20:52:14 +0300

    swiper example

--- script.js
+++ script.js
@@ -13,3 +13,18 @@ var swiper = new Swiper(".mySwiper", {
       clickable: true,
     },
   });
+
+
+  var swiper = new Swiper(".swiper-four-columns", {
+    spaceBetween: 30,
+    slidesPerView: 4,
+    slidesPerGroup: 4,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
