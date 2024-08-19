99b5c4358a1d78db063453704d2752756b5987af
dima-melnik <dmelnyk40@gmail.com>
2024-08-19 22:34:04 +0300

    fix-js-2

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 
+
+
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    effect: "fade",
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
+
   var swiper = new Swiper(".swiper-four-columns", {
     spaceBetween: 30,
     slidesPerView: 4,
