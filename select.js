85413e62f2e9394743768ce63200d15ba92d17ee
dima-melnik <dmelnyk40@gmail.com>
2024-08-31 20:56:36 +0300

    Merge branch 'examples' of https://github.com/mykhatilo-vlad/shoes-shop

--- select.js
+++ select.js
@@ -1,4 +1,15 @@
 
+const smallImages = document.querySelectorAll('.small-img');
+const productImg = document.getElementById('ProductImg');
+
+smallImages.forEach(img => {
+    img.addEventListener('click', function() {
+        productImg.src = this.src;
+    });
+});
+
+
+
 document.querySelectorAll('.dropdown-btn').forEach(function (btn) {
     btn.addEventListener('click', function () {
         const dropdownList = this.nextElementSibling;
@@ -28,89 +39,3 @@ buttonFilter.addEventListener('click', () => {
   formGroup.classList.toggle('form-group--active');
 })
 
-
-
-const smallImages = document.querySelectorAll('.small-img');
-const productImg = document.getElementById('ProductImg');
-
-smallImages.forEach(img => {
-    img.addEventListener('click', function() {
-        productImg.src = this.src;
-    });
-});
-
-
-// cart
-
-
-const plusButtons = document.querySelectorAll('.plus');
-const minusButtons = document.querySelectorAll('.minus');
-
-const totalSumSpan = document.querySelector('#totalSum');
- 
-function updateTotalSum() {
-  let totalSum = 0;
-  const allSums = document.querySelectorAll('.sum');
-  
-  allSums.forEach(sum => {
-    totalSum += parseInt(sum.textContent, 10);
-  });
-
- 
-  totalSumSpan.textContent = totalSum + ' грн';
-}
-
-plusButtons.forEach((plusButton, index) => {
-  plusButton.addEventListener('click', function() {
-    const number = document.querySelectorAll('.number')[index];
-    const sum = document.querySelectorAll('.sum')[index];
-
-    let currentValue = parseInt(number.textContent, 10);
-    number.textContent = currentValue + 1;
-
-    let sumValue = parseInt(sum.textContent, 10);
-    let price = 810; 
-    sum.textContent = sumValue + price;
-
-    updateTotalSum();
-  });
-});
-
-minusButtons.forEach((minusButton, index) => {
-  minusButton.addEventListener('click', function() {
-    const number = document.querySelectorAll('.number')[index];
-    const sum = document.querySelectorAll('.sum')[index];
-
-    let currentValue = parseInt(number.textContent, 10);
-    if (currentValue > 1) {  
-      number.textContent = currentValue - 1;
-
-      let sumValue = parseInt(sum.textContent, 10);
-      let price = 810;  
-      sum.textContent = sumValue - price;
-
-     
-      updateTotalSum();
-    }
-  });
-});
-
-updateTotalSum();
-
-
-document.querySelectorAll('.remove').forEach((removeButton) => {
-    removeButton.addEventListener('click', function() {
-      const productElement = this.closest('.cart-details');  
-  
-      
-      productElement.remove();
-  
-     
-      updateTotalSum();
-    });
-  });
-  
-  updateTotalSum();
-
-
-  
